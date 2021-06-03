1e1b5db55f13cb9f18ca39e97da3997455582612
Rohith P <73927063+Rohith18p@users.noreply.github.com>
2021-06-03 15:30:14 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -1,6 +1,26 @@
 import math
 import unittest
+import random
 
+def wallis(n):
+    pi = 2.0
+    for i in range(n):
+        pi = pi*((4*(i+1)*(i+1))/(4*(i+1)*(i+1)-1))
+    return pi
+
+def monte_carlo(nd):
+    circ_prob = 0.0
+    length = float()
+    centre = [0.0,0.0]
+    dart = [0.0,0.0]
+    for i in range(nd):
+        dart[0] = random.random()
+        dart[1] = random.random()
+        length = math.dist(dart, centre)
+        if length < 1.0:
+            circ_prob += 1.0
+    return 4*circ_prob/nd
+          
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
